990c373f198284b83fd981d0cc8ea8b764a24e1c
DabTheMatt <dabrowskimaciej@protonmail.com>
2022-09-29 14:54:43 +0200

    select unit

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -19,8 +19,20 @@ export default function Board() {
 
     const [units, setUnits] = useState([
         '', '', '', '', '', '', '', '', '', '', 
-        '', 'dog', '', '', '', '', '', '', '', '', 
-        '', 'dog', '', '', '', '', 'cat', '', '', '', 
+        '', {type: 'dog',
+        id: 'd1',
+        name: 'Dog',
+        age: 2,
+        attack: 3,
+        defense: 5,
+        selected: 'no'}, '', '', '', '', '', '', '', '', 
+        '', {type: 'dog',
+        id: 'd2',
+        name: 'Dog',
+        age: 2,
+        attack: 4,
+        defense: 5,
+        selected: 'no'}, '', '', '', '', 'cat', '', '', '', 
         '', '', '', '', '', '', '', 'cat', '', '', 
         '', {type: 'bird',
             id: 'b1',
@@ -33,10 +45,27 @@ export default function Board() {
 
     const [selectedStyle, setSelectedStyle] = useState('');
 
-    const select = (index) => {
-        console.log('clicked', index);
-        selectedStyle ? setSelectedStyle('') : setSelectedStyle('selected');
+    const select = (e, dogId) => {
+        let tempUnits = units;
+        console.log('tempUnits', tempUnits);
+        units.forEach((unit) => {
+            console.log('unit id', unit.id);
+            console.log('dog id', dogId);
+            if(unit.id && unit.id === dogId) {
+
+                unit.selected = 'selected';
+                console.log(unit.selected);
+            } else if (unit.id && unit.id !== dogId){
+                unit.selected = 'no';
+            }
+        })
+        setUnits([...tempUnits]);
     }
+
+
+    const click = () => {
+      console.log('click');
+    };
 
     return (
       <div className="board-wrapper">
@@ -49,18 +78,20 @@ export default function Board() {
         </div>
         <div className="unit-board">
              {units.map(function(unit, key) {
-                if(unit === 'dog') {
+                if(unit.type === 'dog') {
                     return (
                         <Dog 
-                        select={select} 
-                        index={key}
-                        selectedStyle={selectedStyle}
-                        key={key}
-                        />
+                        name={unit.name}
+                        attack={unit.attack}
+                        defense={unit.defense}
+                        id={unit.id}
+                        select={select}
+                        selected={unit.selected}
+                        selectedStyle={selectedStyle}/>
                     )
                 } else if(unit === 'cat') {
                     return (
-                        <Cat />
+                        <Cat onClick={click}/>
                     )
                 } else if(unit.type === 'bird') {
                     return (
--- src/Components/Dog/Dog.js
+++ src/Components/Dog/Dog.js
@@ -2,10 +2,11 @@ import { useState, useRef, useEffect } from "react";
 
 export default function Dog(props) {
 
-
     return (
-        <div className={`dog ${props.selectedStyle}`} onClick={()=>props.select(props.index)}>
-            
+        <div className={`dog ${props.selected}`} onClick={(e)=>props.select(e, props.id)}>
+            <p className="unit-name">{props.name}</p>
+            <p>{props.selected}</p>
+            <p className="unit-stats">{props.attack} / {props.defense}</p>
         </div>
     );
   }
